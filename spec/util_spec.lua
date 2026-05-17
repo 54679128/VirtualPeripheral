d9be795d25435aeb06198c1c35a58af99e57f011
54679128 <a54679128@outlook.com>
2026-05-17 13:49:17 +0800

    test(util_spec): 为`util`模块新增的`readOnly`函数添加测试

--- spec/util_spec.lua
+++ spec/util_spec.lua
@@ -1,3 +1,5 @@
+local util = require("lib.util")
+
 local function unSerializeTable(theTableString)
     local preString = "local theTable;theTable = "
     local afterString = ";return theTable"
@@ -49,4 +51,57 @@ describe("util模块测试", function()
     --     local theTable = unSerializeTable(p.serializeTable(testTable))
     --     assert.Same(theTable, testTable)
     -- end)
+    describe("对于readOnly函数", function()
+        it("可以返回简单表的只读副本", function()
+            local testTable = {
+                a = 1,
+                b = "hello",
+                c = function()
+                    return 8
+                end,
+                d = false,
+                e = coroutine.create(function(...)
+
+                end)
+            }
+            local r = util.readOnly(testTable)
+            assert.is.same(testTable, r)
+            r.a = 2
+            r.b = "get out"
+            r.c = function()
+                return -8
+            end
+            r.d = true
+            r.e = coroutine.create(function(...)
+
+            end)
+            assert.is.same(testTable, r)
+            assert.is.equal(util.readOnly(testTable, true), util.readOnly(testTable, true))
+        end)
+        it("可以返回嵌套表的只读副本", function()
+            local testTable = {
+                a = {
+                    b = 1,
+                    c = "hello"
+                }
+            }
+            local r = util.readOnly(testTable)
+            assert.is.same(testTable, r)
+            r.a = 2
+            assert.is.same(testTable, r)
+            r.a.b = 2
+            r.a.c = "get out"
+            assert.is.same(testTable, r)
+            assert.is.equal(util.readOnly(testTable, true), util.readOnly(testTable, true))
+        end)
+        it("可以返回循环嵌套表的只读副本", function()
+            local testTable = {}
+            testTable.d = testTable
+            local r = util.readOnly(testTable, true) -- 当theTable是循环嵌套表时，必须启用usingCache
+            assert.is.same(testTable, r)
+            r.d = 3
+            assert.is.same(testTable, r)
+            assert.is.equal(util.readOnly(testTable, true), util.readOnly(testTable, true))
+        end)
+    end)
 end)
